84ac10cd9c6eda2aebeb236ad5b2ed8ca91e41e9
manohar <manumanohar1527@gmail.com>
2025-10-17 11:57:03 +0530

    feat: added chart-js library * register chart with the components * refered - https://react-chartjs-2.js.org/examples/line-chart   - https://msemihcelik.medium.com/building-vertical-bar-chart-with-react-chart-js-2-quick-guid-28c466f1d13c

---
+++ src/components/Chart.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line, Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Tooltip,
+  Legend
+);
+
+interface ChartProps {
+  title: string;
+  data: unknown[];
+  type: "line" | "bar" | "multi-line";
+  chartData?: {
+    labels: string[];
+    datasets: unknown[];
+  };
+}
+
+const Chart: React.FC<ChartProps> = ({ title, data, type, chartData }) => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: { display: true, position: "top" as const },
+    },
+    maintainAspectRatio: false,
+  };
+
+  const renderChart = () => {
+    if (!chartData) {
+      return (
+        <div className='chart-placeholder'>
+          <p>{title} Chart</p>
+          <p>Data: {data.length} points</p>
+        </div>
+      );
+    }
+
+    if (type === "bar") {
+      return (
+        <Bar
+          options={options}
+          data={chartData as any}
+        />
+      );
+    }
+
+    return (
+      <Line
+        options={options}
+        data={chartData as any}
+      />
+    );
+  };
+
+  return (
+    <div className='chart-container'>
+      <h3>{title}</h3>
+      <div style={{ height: 300 }}>{renderChart()}</div>
+    </div>
+  );
+};
+
+export default Chart;
